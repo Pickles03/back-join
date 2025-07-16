e3769ae4cbde663d83f766496f8c290c60d8605a
Pickles03 <elizaveta.kris@gmail.com>
2025-07-16 14:09:25 +0200

    ch2s20

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: String,
-    completed: Boolean
+    title: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 }, { timestamps: true });
 
 const Task = mongoose.model('Task', TaskSchema);
